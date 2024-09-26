86cf3a1d9e46f312d7fab89d236ee4bc2b164b0a
PaoloIncollu <paoloinco@gmail.com>
2024-09-26 15:14:32 +0200

    esercizio completato

--- index.php
+++ index.php
@@ -4,8 +4,46 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Badwords</title>
+
+    <link rel= "stylesheet"  href= "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"  integrity= "sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"  crossorigin= "anonimo" >
+
 </head>
 <body>
+
+<div class="p-5 m-5 border border-2 w-25">
+    <form action="./result.php" method="POST" > 
+
+        <div class="d-flex justify-content-between align-items-center w-100 mb-3">
+
+            <label for="text" class="me-3">
+                Inserisci testo a caso
+            </label>
+
+            <textarea name="text" id="text" >
+            </textarea>
+        </div>
+       
+        <div class="d-flex justify-content-between align-items-center  w-100">
+
+            <label for="bad-word">
+                Inserisci parola da censurare
+            </label>
+
+            
+            <input name="badWord" type="text" id="bad-word">
+        </div>
+        
+        <div class="d-flex justify-content-end w-100 pt-3">
+
+
+            <button type="submit">
+                Invia
+            </button>
+        </div>
+
+        
+    </form>
+</div>
     
 </body>
 </html>
--- result.php
+++ result.php
@@ -4,8 +4,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Results</title>
+
+    <link rel= "stylesheet"  href= "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"  integrity= "sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"  crossorigin= "anonimo" >
 </head>
 <body>
     
+    <div class="p-5 m-5 border border-2 w-25">
+        <h2>
+            Il testo inserito è:
+        </h2>
+
+        <p>
+            <?php echo $_POST['text']?>
+        </p>
+        <h4>
+            Lunghezza del paragrafo: <?php echo strlen($_POST['text']); ?>
+        </h4>
+
+        <p>
+            <?php echo 
+                    str_replace($_POST['badWord'], '***'  , $_POST['text']);
+            ?>
+        </p>
+
+        <h4>
+            Lunghezza del paragrafo: <?php echo strlen(str_replace($_POST['badWord'], '***'  , $_POST['text'])); ?>
+        </h4>
+
+    </div>
 </body>
 </html>
